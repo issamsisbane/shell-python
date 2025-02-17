9ee655e6e1fb00ad70317992316e89ecd464dda2
Issam SISBANE <issamsnz@proton.me>
2025-02-17 21:56:37 +0100

    feat: add search in PATH for type command

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import sys
-
+import os
 
 def main():
     
-    builtins = ["exit", "echo", "type"]
+    BUILTINS = ["exit", "echo", "type"]
 
     while(True):
         sys.stdout.write("$ ")
@@ -20,13 +20,29 @@ def main():
             case "echo":
                 print(" ".join(command_parts[1:]))
             case "type":
-                if len(command_parts) > 1:
-                    if(command_parts[1] in builtins):
+
+                if len(command_parts) > 1:    
+            
+                    if(command_parts[1] in BUILTINS):
                         print(f"{command_parts[1]} is a shell builtin")
                     else:
-                        print(f"{command_parts[1]}: not found")
+                        found_command_in_path = search_command_in_path(command_parts[1])
+                        if found_command_in_path is not None:
+                                print(f"{command_parts[1]} is {found_command_in_path}")
+                        else:
+                            print(f"{command_parts[1]}: not found")
             case _:
                 print(f"{command}: command not found")
+
+def search_command_in_path(command_to_find):
+    PATH = os.getenv("PATH")
+
+    folders_to_search = PATH.split(":")
+    for folder_to_search in folders_to_search:
+        if command_to_find in os.listdir(folder_to_search):
+            return f"{folder_to_search}/{command_to_find}"
+    return None
+
 
 if __name__ == "__main__":
     main()
