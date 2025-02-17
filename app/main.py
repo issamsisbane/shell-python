5c7338c1bdb11c2e06b4324dd9cbb5a96cf67223
Issam SISBANE <issamsnz@proton.me>
2025-02-17 21:08:47 +0100

    feat: add the exit command with a code possible

--- app/main.py
+++ app/main.py
@@ -8,6 +8,12 @@ def main():
 
         command = input()
 
+        command_parts = command.split(" ")
+
+        if (command_parts[0] == "exit"):
+            if len(command_parts) > 1:
+                sys.exit(command_parts[1])
+
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
