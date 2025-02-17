330f4332dd84b62dbbddefcfc6378486dd246056
Issam SISBANE <issamsnz@proton.me>
2025-02-17 22:06:01 +0100

    refactor: create functions for each command

--- app/main.py
+++ app/main.py
@@ -1,38 +1,27 @@
 import sys
 import os
 
-def main():
-    
+def exit(command_parts):
+    if len(command_parts) > 1:
+        sys.exit(int(command_parts[1]))
+    sys.exit(0)
+
+def echo(command_parts):
+    print(" ".join(command_parts[1:]))
+
+def type(command_parts):
+
     BUILTINS = ["exit", "echo", "type"]
-
-    while(True):
-        sys.stdout.write("$ ")
-
-        command = input()
-
-        command_parts = command.split(" ")
-
-        match command_parts[0]:
-            case "exit":
-                if len(command_parts) > 1:
-                    sys.exit(int(command_parts[1]))
-                sys.exit(0)
-            case "echo":
-                print(" ".join(command_parts[1:]))
-            case "type":
-
-                if len(command_parts) > 1:    
-            
-                    if(command_parts[1] in BUILTINS):
-                        print(f"{command_parts[1]} is a shell builtin")
-                    else:
-                        found_command_in_path = search_command_in_path(command_parts[1])
-                        if found_command_in_path is not None:
-                                print(f"{command_parts[1]} is {found_command_in_path}")
-                        else:
-                            print(f"{command_parts[1]}: not found")
-            case _:
-                print(f"{command}: command not found")
+    
+    if len(command_parts) > 1:    
+        if(command_parts[1] in BUILTINS):
+            print(f"{command_parts[1]} is a shell builtin")
+        else:
+            found_command_in_path = search_command_in_path(command_parts[1])
+            if found_command_in_path is not None:
+                    print(f"{command_parts[1]} is {found_command_in_path}")
+            else:
+                print(f"{command_parts[1]}: not found")
 
 def search_command_in_path(command_to_find):
     PATH = os.getenv("PATH")
@@ -43,6 +32,24 @@ def search_command_in_path(command_to_find):
             return f"{folder_to_search}/{command_to_find}"
     return None
 
+def main():
+    
+    while(True):
+        sys.stdout.write("$ ")
+
+        command = input()
+
+        command_parts = command.split(" ")
+
+        match command_parts[0]:
+            case "exit":
+                exit(command_parts)
+            case "echo":
+                echo(command_parts)
+            case "type":
+                type(command_parts)
+            case _:
+                print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
